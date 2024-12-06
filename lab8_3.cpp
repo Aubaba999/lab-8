3c16e5a4ef6ec69da0733ff8ddf5258e433d3059
Aubaba999 <sutjii1989@gmail.com>
2024-12-06 16:26:29 +0700

    lab 8

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,53 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+#include<string>
+
+int main (){
+   int old,tall,headprice;
+    cout<<"Enter your age: ";
+    cin>>old;
+    
+if(old<=25){
+    cout<<"Enter your height: ";
+    cin>>tall;
+   if(tall<100){
+    cout<<"Your character = Chopper";
+   }else if(tall<180){
+    cout<<"Your character = Usopp";
+   }else {
+    cout<<"Enter your bounty: ";
+    cin>>headprice;
+   if(headprice>1100000000){
+    cout<<"Your character = Zoro";
+   }
+    else{
+    cout<< "Your character = Sanji";
+   }
+   }
+    
+
+}else   if(old<=60){
+        cout<<"Enter your bounty: ";
+        cin>>headprice;
+        if(headprice>500000000){
+            cout<<"Your character = Jinbe";
+        }else{
+            cout<<"Your character = Franky";
+        }
+        }
+    else{
+        cout<<"Your character = Brook";
+    }
+}
+    
+
+
+
+
+
+
+
+
+
+
+;
